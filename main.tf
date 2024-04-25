97f72992395b340e24ba4e8b174e437c7fdf5d02
Eric Castoldi <eric.castoldi@gmail.com>
2024-04-24 22:18:42 -0300

    Comment tags

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 resource "aws_instance" "web" {
-    instance_type = "t2.micro"
-    ami           = "ami-09b4b74c"
+  instance_type = "t2.micro"
+  ami           = "ami-09b4b74c"
 
-    tags = {
-        CostCenter = "Sales"
-    }
+  # tags = {
+  #     CostCenter = "Sales"
+  #}
 }
