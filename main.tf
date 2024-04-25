368edba0fa0022fb7f9de5f6a43974e087cb1b46
Eric Castoldi <eric.castoldi@gmail.com>
2024-04-24 22:48:10 -0300

    Add tags

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   ami           = "ami-09b4b74c"
 
-  # tags = {
-  #     CostCenter = "Sales"
-  #}
+  tags = {
+    CostCenter = "Sales"
+  }
 }
